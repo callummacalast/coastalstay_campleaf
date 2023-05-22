75d62647bc2d1b440c4deede36b24ee822cb3f52
Callum Macalast <calmacalast@gmail.com>
2023-05-22 15:25:18 +0100

    [IMP] goat section

--- resources/views/goats/index.blade.php
+++ resources/views/goats/index.blade.php
@@ -1,13 +1,13 @@
 <x-guest-layout>
-    <section class=" text-gray-100">
+    <section class="dark:bg-gray-800 dark:text-gray-100 pt-5">
         <div class="container flex flex-col-reverse mx-auto lg:flex-row">
-            <div class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 bg-sky-400 text-gray-900">
+            <div class="flex flex-col justify-center px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-sky-400 dark:text-gray-900">
                 <div class="flex space-x-2 sm:space-x-4">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
                     </svg>
                     <div class="space-y-2">
-                        <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+                        <p class="text-lg font-medium leading-snug">Goat Walking</p>
                         <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
                     </div>
                 </div>
@@ -16,7 +16,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
                     </svg>
                     <div class="space-y-2">
-                        <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+                        <p class="text-lg font-medium leading-snug">Our Goats</p>
                         <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
                     </div>
                 </div>
@@ -25,14 +25,14 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
                     </svg>
                     <div class="space-y-2">
-                        <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+                        <p class="text-lg font-medium leading-snug">Something here</p>
                         <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
                     </div>
                 </div>
             </div>
-            <div class="lg:w-1/2 xl:w-3/5 ">
-                <div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
-                    <img src="https://www.coastalstay.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffarm.068326e6.png&w=1200&q=75" alt="" class="rounded-lg shadow-lg bg-gray-500 aspect-video sm:min-h-96">
+            <div class="lg:w-1/2 xl:w-3/5 h-1/2 dark:bg-gray-800">
+                <div class="flex items-center justify-center p-4 md:p-8 lg:p-12 h-1/2">
+                    <img src="{{ asset('storage/images/nugget_goat.jpeg') }}" alt="" class="rounded-lg shadow-lg dark:bg-gray-500 sm:min-h-96 h-1/2">
                 </div>
             </div>
         </div>
